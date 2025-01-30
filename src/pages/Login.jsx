018d291fde1e5943d5e6ef2d0167a8334be52bef
agonhyseni <hyseniagon@gmail.com>
2025-01-30 19:22:07 +0100

    Improve login screen

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,14 @@
-import { Button, Container, Typography } from '@mui/material';
+import {
+  Box,
+  Button,
+  CircularProgress,
+  Container,
+  Typography,
+} from '@mui/material';
 import { signInWithPopup } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-
+import googleIcon from '../assets/google.svg'; // Import the Google icon
 import { auth, googleProvider } from '../services/firebase.js';
 
 const Login = () => {
@@ -29,13 +35,25 @@ const Login = () => {
         </Box>
       ) : (
         <>
+          <img
+            src="https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExNTFpZzBmMDV5Mzg0YWJob2Ewb3YzYXN4eDdkcXgwazV3MjlpdWh3biZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/hkTXRkfarShjLBQH0v/giphy.gif"
+            alt="hec e hec"
+            style={{ width: '100px', heigh: '100px', marginBottom: '20px' }}
+          />
           <Typography variant="h4" gutterBottom>
-            Welcome to hecehec 🚶
+            hec e hec
           </Typography>
           <Button
-            variant="contained"
-            color="primary"
+            variant="outlined"
+            color="transparent" // Set button color to white
             onClick={signInWithGoogle}
+            startIcon={
+              <img
+                src={googleIcon}
+                alt="Google"
+                style={{ width: 24, height: 24 }}
+              />
+            } // Use Google icon
           >
             Sign In with Google
           </Button>
